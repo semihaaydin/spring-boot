071f9ad659d7f910f2270da5d50a2eed5d8d30e2
Semiha Aydın <semiha.aydinn@gmail.com>
2023-01-13 10:46:11 +0300

    Spring Boot Kafka Architecture

---
+++ spring-boot-kafka/src/main/java/com/saydin/KafkaListenerService.java
@@ -0,0 +1,23 @@
+package com.saydin;
+
+import com.saydin.dto.KMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaListenerService {
+
+    @KafkaListener(
+            topics = "${saydin.kafka.topic}",
+            groupId = "${saydin.kafka.group.id}"
+    )
+    public void listen(@Payload KMessage message) {
+        log.info("Message received.. MessageID : {} Message: {} Date : {}",
+                message.getId(),
+                message.getMessage(),
+                message.getMessageDate());
+    }
+}
